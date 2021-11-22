6e44da7fa16326d99674e22498763d21e1ab5aa9
Antisedative <88254939+Antisedative@users.noreply.github.com>
2021-11-22 19:39:21 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('.plans__card-starting').mouseover(function () {
+    $(this).addClass('scaled');
+    $('.plans__card-business').addClass('business-scale');
+});
+$('.plans__card-starting').mouseout(function () {
+    $(this).removeClass('scaled');
+    $('.plans__card-business').removeClass('business-scale');
+});
+$('.plans__card-vip').mouseover(function () {
+    $(this).addClass('scaled');
+    $('.plans__card-business').addClass('business-scale');
+});
+$('.plans__card-vip').mouseout(function () {
+    $(this).removeClass('scaled');
+    $('.plans__card-business').removeClass('business-scale');
+});
