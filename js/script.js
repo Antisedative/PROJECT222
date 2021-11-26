c34b911f17707f2e6b669fc5edebd2c643b9f8d6
Antisedative <88254939+Antisedative@users.noreply.github.com>
2021-11-26 22:52:41 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,84 +1,68 @@
 // ТАРИФЫ
 
 $(document).ready(function () {
-    $('.item__start').mouseover(function () {
+    $('.plans__card-starting').mouseover(function () {
         $(this).addClass('scaled');
-        $('.item__business').addClass('business-scale');
+        $('.plans__card-business').addClass('business-scale');
     });
-    $('.item__start').mouseout(function () {
+    $('.plans__card-starting').mouseout(function () {
         $(this).removeClass('scaled');
-        $('.item__business').removeClass('business-scale');
+        $('.plans__card-business').removeClass('business-scale');
     });
-    $('.item__vip').mouseover(function () {
+    $('.plans__card-vip').mouseover(function () {
         $(this).addClass('scaled');
-        $('.item__business').addClass('business-scale');
+        $('.plans__card-business').addClass('business-scale');
     });
-    $('.item__vip').mouseout(function () {
+    $('.plans__card-vip').mouseout(function () {
         $(this).removeClass('scaled');
-        $('.item__business').removeClass('business-scale');
+        $('.plans__card-business').removeClass('business-scale');
     });
 
-// ОТЗЫВЫ
 
-    $('.slider').on(`init reInit`, function (event, slick) {
+    // ОТЗЫВЫ
+
+    $('.reviews__slider').on(`init reInit`, function (event, slick) {
         $('.slick-slide-num-current').text('0' + 1 + ' '); // ' / ' + '0' + slick.slideCount
     })
-    $('.slider').on(`afterChange`, function (event, slick, currentSlide, nextSlide) {
+    $('.reviews__slider').on(`afterChange`, function (event, slick, currentSlide, nextSlide) {
         $('.slick-slide-num-current').text('0' + (currentSlide + 1) + ' '); // + ' / 0' + slick.slideCount
     })
-    $('.slider').slick({
+    $('.reviews__slider').slick({
+        arrows: true,
         dots: false,
         infinite: true,
         speed: 500,
         fade: true,
         adaptiveHeight: true,
-        cssEase: 'linear',
+        easing: 'ease',
         appendArrows: '.slick__arrows',
-        prevArrow: '<button type="button" data-role="none" class="slick-prev slick-arrow" aria-label="Previous"></button>',
-        nextArrow: '<button type="button" data-role="none" class="slick-next slick-arrow" aria-label="Next"></button>',
+        prevArrow: '<button data-role="none" class="slick-prev slick-arrow" aria-hidden="true"></button>',
+        nextArrow: '<button type="button" data-role="none" class="slick-next slick-arrow" aria-label=""></button>',
+
+    });
+
+    // ПАРТНЕРЫ
+
+    $('.partners__slider-1').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        easing: 'ease',
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        speed: 1500,
+    });
+
+    $('.partners__slider-2').slick({
+        arrows: false,
+        dots: false,
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        easing: 'ease',
+        autoplay: true,
+        autoplaySpeed: 2700,
+        speed: 2000,
     });
 });
-
-// ПАРТНЕРЫ
-
-// $(document).ready(function () {
-//     $('.partners-slider-1').slick({
-//         arrows: false,
-//         dots: false,
-//         slidesToShow: 5,
-//         slidesToScroll: 1,
-//         centerMode: true,
-//         focusOnSelect: true,
-//         autoplay: true,
-//         autoplaySpeed: 2000,
-//         speed: 500,
-//         responsive: [{
-//             breakpoint: 480,
-//             settings: {
-//                 slidesToShow: 2,
-//                 slidesToScroll: 1,
-//                 centerMode: false,
-//                 variableWidth: true,
-//             }
-//         }],
-//     });
-//     $('.partners-slider-2').slick({
-//         arrows: false,
-//         dots: false,
-//         slidesToShow: 6,
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         autoplaySpeed: 2000,
-//         speed: 500,
-//         responsive: [{
-//             breakpoint: 480,
-//             settings: {
-//                 slidesToShow: 3,
-//                 slidesToScroll: 1,
-//                 centerMode: true,
-//                 focusOnSelect: true,
-//                 variableWidth: true,
-//             }
-//         }],
-//     });
-// });
